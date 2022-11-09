f0be7d059f601ade2949a700fe246a9657d2d54b
Sarvinoz <fullstackdevelop2023@gmail.com>
2022-11-09 10:24:53 +0500

    some changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import "./Footer.scss";
+import FooterTop from "./FooterTop";
 
 const Footer = () => {
   return <>
     <footer className="footer">
-      <div className="container">
-        <div className="footer__hero">
-
+      <FooterTop />
+      <div className="footer__middle">
+        <div className="container">
+          <div className="footer__middle--hero"></div>
+        </div>
+      </div>
+      <div className="footer__bottom">
+        <div className="container">
+          <div className="footer__bottom--hero"></div>
         </div>
       </div>
     </footer></>;
---
+++ src/components/Footer/FooterTop.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Footer.scss";
+
+const FooterTop = () => {
+  return <>
+    <div className="footer__top">
+      <div className="container">
+        <div className="footer__top--hero">
+          <h2 className="footer__top--logo">BE IN TOUCH WITH US:</h2>
+          <div className="footer__top--email">
+            <input type="text" placeholder="Enter your email" className="footer__top--input" />
+            <button className="footer__top--btn">Join Us</button>
+          </div>
+          <div className="footer__top--icons">
+            <ul className="header__right--list footer__top--list">
+              <li className="header__right--item">
+                <a href="#">
+                  <i class="fa-brands fa-facebook header__right--icon"></i>
+                </a>
+              </li>
+              <li className="header__right--item">
+                <a href="#">
+                  <i class="fa-brands fa-twitter header__right--icon"></i>
+                </a>
+              </li>
+              <li className="header__right--item">
+                <a href="#">
+                  <i class="fa-brands fa-instagram header__right--icon"></i>
+                </a>
+              </li>
+              <li className="header__right--item">
+                <a href="#">
+                  <i class="fa-brands fa-pinterest header__right--icon"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </>;
+};
+
+export default FooterTop;
