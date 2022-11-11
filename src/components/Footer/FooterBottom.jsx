7e27ccba4300fd6beff4e326494e98b9a33526e6
Sarvinoz <fullstackdevelop2023@gmail.com>
2022-11-11 16:14:57 +0500

    responsive footer

--- src/components/Footer/FooterBottom.jsx
+++ src/components/Footer/FooterBottom.jsx
@@ -3,25 +3,34 @@ import "./Footer.scss";
 import { NavLink } from "react-router-dom";
 
 const FooterBottom = () => {
-    return <>
-        <div className="footer__bottom">
-            <div className="container">
-                <div className="footer__bottom__hero">
-                    <div className="footer__bottom--logo">
-                        Copyright © 2032 all rights reserved
-                    </div>
-                    <div className="footer__bottom--icons">
-                        <NavLink to="#"><i class="fa-brands fa-cc-visa footer__bottom--icon"></i></NavLink>
-                        <NavLink to=""><i class="fa-brands fa-cc-paypal footer__bottom--icon"></i></NavLink>
-                        <NavLink to=""><i class="fa-brands fa-cc-mastercard footer__bottom--icon"></i></NavLink>
-                    </div>
-                    <div className="footer__middle--author">
-                        Designed by <span className="footer__bottom--span">Oleh Chabanov</span>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="footer__bottom">
+        <div className="container">
+          <div className="footer__bottom__hero">
+            <div className="footer__bottom--logo">
+              Copyright © 2032 all rights reserved
             </div>
+            <div className="footer__bottom--icons">
+              <NavLink to="#">
+                <i class="fa-brands fa-cc-visa footer__bottom--icon"></i>
+              </NavLink>
+              <NavLink to="">
+                <i class="fa-brands fa-cc-paypal footer__bottom--icon"></i>
+              </NavLink>
+              <NavLink to="">
+                <i class="fa-brands fa-cc-mastercard footer__bottom--icon"></i>
+              </NavLink>
+            </div>
+            <div className="footer__bottom--author">
+              Designed by{" "}
+              <span className="footer__bottom--span">Oleh Chabanov</span>
+            </div>
+          </div>
         </div>
-    </>;
+      </div>
+    </>
+  );
 };
 
 export default FooterBottom;
